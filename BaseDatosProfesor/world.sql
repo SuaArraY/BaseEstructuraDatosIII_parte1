e1c29fa7f167451c4cea8ccdae5616fd3238cbbe
Yossec <1313178@senati.pe>
2022-11-18 18:41:44 -0500

    nombre de las tablas en español

--- BaseDatosProfesor/world.sql
+++ BaseDatosProfesor/world.sql
@@ -30,10 +30,10 @@ USE `world`;
 -- Table structure for table `city`
 --
 
-DROP TABLE IF EXISTS `city`;
+DROP TABLE IF EXISTS `ciudad`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `city` (
+CREATE TABLE `ciudad` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Name` char(35) NOT NULL DEFAULT '',
   `CountryCode` char(3) NOT NULL DEFAULT '',
@@ -4136,10 +4136,10 @@ commit;
 -- Table structure for table `country`
 --
 
-DROP TABLE IF EXISTS `country`;
+DROP TABLE IF EXISTS `pais`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `country` (
+CREATE TABLE `pais` (
   `Code` char(3) NOT NULL DEFAULT '',
   `Name` char(52) NOT NULL DEFAULT '',
   `Continent` enum('Asia','Europe','North America','Africa','Oceania','Antarctica','South America') NOT NULL DEFAULT 'Asia',
@@ -4410,10 +4410,10 @@ commit;
 -- Table structure for table `countrylanguage`
 --
 
-DROP TABLE IF EXISTS `countrylanguage`;
+DROP TABLE IF EXISTS `lenguaje_pais`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `countrylanguage` (
+CREATE TABLE `lenguaje_pais` (
   `CountryCode` char(3) NOT NULL DEFAULT '',
   `Language` char(30) NOT NULL DEFAULT '',
   `IsOfficial` enum('T','F') NOT NULL DEFAULT 'F',
